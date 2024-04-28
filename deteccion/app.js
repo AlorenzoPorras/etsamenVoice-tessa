da86075a16d966f508de228bb5344514e9e1ecce
Adrielo <alorenzoporras@gmail.com>
2024-04-28 17:27:57 -0600

    ajio

--- deteccion/app.js
+++ deteccion/app.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Obtener referencias a los elementos HTML necesarios
     const orderResultDiv = document.getElementById('orderResult');
+    const usuario = localStorage.getItem('usuarioNombre') || "Usuario Desconocido"; // Obtiene el nombre del usuario desde localStorage
+    const fechaHora = obtenerFechaHoraMexico();
 
     // Función para iniciar el reconocimiento de voz
     const startRecognition = () => {
@@ -17,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 console.log('Orden identificada:', result);
 
                 // Verificar si la orden contiene la palabra clave "Tessa"
-                if (result.includes("tessa")) {
+                if (result.includes("tesa")) {
                     // Procesar la orden identificada
                     processCommand(result);
                 }
@@ -39,101 +41,30 @@ document.addEventListener('DOMContentLoaded', function () {
     };
 
     // Función para procesar la orden identificada por voz
-    const processCommand = (result) => {
-        // Procesar cada comando específico
-        if (result.includes("enciende la luz de la recámara")) {
+    function processCommand(result) {
+        if (result.includes("enciende luz de recámara")) {
             orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-            // Cambiar la imagen de la luz de la recámara a encendida
-            document.getElementById('imagenRecamara').src = 'luz_encendida.jpg';
-            insertarJson("Encender la luz de la recámara");
-        } else if (result.includes("apaga la luz de la recámara")) {
+            insertarJson(usuario, fechaHora, "Encender la luz de la recámara");
+        } else if (result.includes("apaga luz de recámara")) {
             orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-            // Cambiar la imagen de la luz de la recámara a apagada
-            document.getElementById('imagenRecamara').src = 'luz_apagada.jpg';
-            insertarJson("Apagar la luz de la recámara");
-            
-        } else if (result.includes("enciende la luz de la sala")) {
-            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-            // Cambiar la imagen de la luz de la sala a encendida
-            document.getElementById('imagenSala').src = 'luz_encendida.jpg';
-            insertarJson("Encender la luz de la sala");
-        } else if (result.includes("apaga la luz de la sala")) {
-            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-            // Cambiar la imagen de la luz de la sala a apagada
-            document.getElementById('imagenSala').src = 'luz_apagada.jpg';
-            insertarJson("Apagar la luz de la sala");
-
-        } else if (result.includes("enciende las luces del jardín")) {
-            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-            // Cambiar la imagen de las luces del jardín a encendidas
-            document.getElementById('imagenJardin').src = 'luces_encendidas.jpg';
-            insertarJson("Encender las luces del jardín");
-        } else if (result.includes("apaga las luces del jardín")) {
-            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-            // Cambiar la imagen de las luces del jardín a apagadas
-            document.getElementById('imagenJardin').src = 'luces_apagadas.jpg';
-            insertarJson("Apagar las luces del jardín");
-
-        } else if (result.includes("enciende el ventilador")) {
-            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-            // Cambiar la imagen del ventilador a encendido
-            document.getElementById('imagenVentilador').src = 'ventilador_encendido.jpg';
-            insertarJson("Encender el ventilador");
-        } else if (result.includes("apaga el ventilador")) {
-            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-            // Cambiar la imagen del ventilador a apagado
-            document.getElementById('imagenVentilador').src = 'ventilador_apagado.jpg';
-            insertarJson("Apagar el ventilador");
-
-        } else if (result.includes("abre las cortinas")) {
-            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-            // Cambiar la imagen de las cortinas a abiertas
-            document.getElementById('imagenCortinas').src = 'cortinas_abiertas.jpg';
-            insertarJson("Abrir las cortinas");
-        } else if (result.includes("cierra las cortinas")) {
-            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-            // Cambiar la imagen de las cortinas a cerradas
-            document.getElementById('imagenCortinas').src = 'cortinas_cerradas.jpg';
-            insertarJson("Cerrar las cortinas");
-
-        } else if (result.includes("activa la alarma de la casa")) {
-            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-            // Cambiar la imagen de la alarma a activada
-            document.getElementById('imagenAlarma').src = 'alarma_activada.jpg';
-            insertarJson("Activar la alarma de la casa");
-        } else if (result.includes("desactiva la alarma de la casa")) {
-            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-            // Cambiar la imagen de la alarma a desactivada
-            document.getElementById('imagenAlarma').src = 'alarma_desactivada.jpg';
-            insertarJson("Desactivar la alarma de la casa");
-
-        } else if (result.includes("enciende las cámaras de seguridad")) {
-            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-            // Cambiar la imagen de las cámaras de seguridad a encendidas
-            document.getElementById('imagenCamaras').src = 'camaras_encendidas.jpg';
-            insertarJson("Encender las cámaras de seguridad");
-        } else if (result.includes("apaga las cámaras de seguridad")) {
-            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-            // Cambiar la imagen de las cámaras de seguridad a apagadas
-            document.getElementById('imagenCamaras').src = 'camaras_apagadas.jpg';
-            insertarJson("Apagar las cámaras de seguridad");
-            
+            insertarJson(usuario, fechaHora, "Apagar la luz de la recámara");
         } else if (result.includes("ir a monitoreo")) {
             orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
             // Redireccionar a la página de monitoreo
             window.location.href = '../monitoreo/index.html';
         }
-        
-    };
+
+        // Agrega más condiciones según sea necesario para otros comandos
+    }
 
     // Función para enviar los datos a la API
-    function insertarJson(ingresos) {
+    function insertarJson(usuario, fechaHora, orden) {
         return fetch('https://6614da0e2fc47b4cf27d3ce0.mockapi.io/Tessa', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ ingresos })
+            body: JSON.stringify({ usuario, fechaHora, orden })
         })
         .then(response => {
             if (!response.ok) {
@@ -143,6 +74,13 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .then(data => console.log('Subido exitosamente:', data))
         .catch(error => console.error('Error:', error));
+    }
+
+    // Función para obtener la fecha y hora actual en México
+    function obtenerFechaHoraMexico() {
+        const fechaHoraActual = new Date();
+        const options = { timeZone: 'America/Mexico_City' };
+        return fechaHoraActual.toLocaleString('es-MX', options);
     }
 
     // Iniciar el ciclo de reconocimiento de voz automáticamente
